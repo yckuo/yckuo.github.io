c4b7cbf6375c0ece2b0df8d12641597c938a0fc8
U-DESKTOP-L3TJHER\yckuo <kokokoko@gmail.com>
2017-01-12 01:19:35 -0800

    after fading out, then remove caption

--- yckuo.js
+++ yckuo.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
             return;
         }
         $('.modal').fadeOut(400, function() {
+            $('.caption').text('');
         });
-        $('.caption').text('');
     });
 });
