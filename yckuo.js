8bd5b6364621f764feb05d854c89e34521644dd7
U-DESKTOP-L3TJHER\yckuo <kokokoko@gmail.com>
2017-01-11 22:48:55 -0800

    test jquery click event

--- yckuo.js
+++ yckuo.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
     $('.img').click(function() {
         $('.modal-content').attr('src', $(this).attr('src'));
         $('.modal').modal('show');
+        window.alert('test');
     });
 });
