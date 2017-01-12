6292a6413da81480778da0d0225bfc4788ba3f5c
U-DESKTOP-L3TJHER\yckuo <kokokoko@gmail.com>
2017-01-12 01:17:37 -0800

    change clear caption sequence before/after fade in/out

--- yckuo.js
+++ yckuo.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
     $('.img').click(function() {
-        $('.modal-content').attr('src', $(this).attr('src'));
+        $('.modal-content').attr('src', $(this).attr('src')); 
+        $('.caption').text($(this).attr('alt'));
         $('.modal').fadeIn(400, function() {
         });
-        $('.caption').text($(this).attr('alt'));
     });
 
     $('.modal').click(function(e) {
         if (e.target != this) {
             return;
         }
-        $('.caption').text('');
         $('.modal').fadeOut(400, function() {
         });
+        $('.caption').text('');
     });
 });
