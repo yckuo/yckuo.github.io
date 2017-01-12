d9d089496f440f110dc53834f2682aa11b6e7fd6
U-DESKTOP-L3TJHER\yckuo <kokokoko@gmail.com>
2017-01-11 23:08:50 -0800

    try jquery modal

--- yckuo.js
+++ yckuo.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
     $('.img').click(function() {
-        /*$('.modal-content').attr('src', $(this).attr('src'));
-        $('.modal').modal('show');*/
+        $('.modal-content').attr('src', $(this).attr('src'));
+        $('.modal').css('display', 'block');
+        $('.caption').text($(this).attr('alt'));
+        /*$('.modal').modal('show');*/
         alert('test');
     });
 });
