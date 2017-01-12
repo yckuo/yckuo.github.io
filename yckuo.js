97cb3ffeadfd5d5fb03bb9260e6762bede2a041e
U-DESKTOP-L3TJHER\yckuo <kokokoko@gmail.com>
2017-01-11 23:18:17 -0800

    change close icon class to match css definition; add close modal

--- yckuo.js
+++ yckuo.js
@@ -3,7 +3,10 @@ $(document).ready(function() {
         $('.modal-content').attr('src', $(this).attr('src'));
         $('.modal').css('display', 'block');
         $('.caption').text($(this).attr('alt'));
-        /*$('.modal').modal('show');*/
         alert('test');
+    });
+
+    $('.close').click(function() {
+        $('.modal').css('display', 'none');
     });
 });
